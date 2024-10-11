9812e894530f08412d2b7f77a7ef92a6eb11af48
vhari01 <vhari01@student.ubc.ca>
2024-10-10 21:21:23 -0700

    Created a testScheduler class

---
+++ src/test/model/TestScheduler.java
@@ -0,0 +1,36 @@
+package model;
+
+import org.junit.Test;
+
+public class TestScheduler extends TestPatient {
+
+    @Test
+    void testAddPatient(){
+
+    }
+
+    @Test
+    void testCancelAppointment(){
+
+    }
+
+    @Test
+    void testNonExistingAppointmentCancel(){
+
+    }
+
+    @Test
+    void testSortPatientsByPriority(){
+
+    }
+
+    @Test
+    void testTreatNextPatient(){
+
+    }
+
+    @Test
+    void testTreatPatientWhenListEmpty(){
+        
+    }
+}
