bff2db0c3ff453fa8a608326648952281233706c
vhari01 <vhari01@student.ubc.ca>
2024-10-10 17:29:46 -0700

    Created a Test class for patient

---
+++ src/test/model/TestPatient.java
@@ -0,0 +1,32 @@
+package model;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class TestPatient {
+    private Patient p1;
+    private Patient p2;
+    private Patient p3;
+
+    @BeforeEach
+    void runBefore(){
+        
+    }
+
+    @Test
+    void testConstructor() {
+        
+
+
+
+
+    } 
+
+}
