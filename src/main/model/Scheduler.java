4c3da5733805af42bad0f818bd1e91514454ef1e
vhari01 <vhari01@student.ubc.ca>
2024-10-10 17:58:11 -0700

    Created a shceduler class and added desired methods without stubs

---
+++ src/main/model/Scheduler.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Scheduler {
+
+
+    public Scheduler(){
+
+    }
+
+    public void addPatient(Patient patient){
+
+    }
+
+    public void cancelAppointment(){
+
+    }
+
+    public void sortPatientsByPriority(){
+         
+    }
+
+    public void treatNextPatient(){
+
+    }
+
+    public void printListOfPatients(){
+
+    }
+    
+    
+}
+
