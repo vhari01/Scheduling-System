e2c61222db78cf0e482e81d5e9bbcf8d6f427eda
vhari01 <vhari01@student.ubc.ca>
2024-10-10 20:55:40 -0700

    Tested all the methods of the Patient class

--- src/test/model/TestPatient.java
+++ src/test/model/TestPatient.java
@@ -2,6 +2,7 @@ package model;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.LocalDate;
 
@@ -14,12 +15,14 @@ public class TestPatient {
     private Patient p1;
     private Patient p2;
     private Patient p3;
+    private Patient p4;
 
     @BeforeEach
     void runBefore(){
         p1 = new Patient("Max",23, true, 3, "Orthopedic", LocalDate.of(2024, 10, 13));
         p2 = new Patient("Jack",50, false, 5, "Cardiologist", LocalDate.of(2024, 10, 13));
-        p3 = new Patient("Gil",38, true, 1, "Dentist", LocalDate.of(2024, 10, 13));
+        p3 = new Patient("Gil",38, true, 3, "Cardiologist", LocalDate.of(2024, 10, 13));
+        p4 = new Patient("cacy",32, true, 3, "Orthopedic", LocalDate.of(2024, 10, 13));
     }
 
 
@@ -32,6 +35,37 @@ public class TestPatient {
         assertEquals(3, p1.getLevelOfEmergency());
         assertEquals(LocalDate.of(2024, 10, 13), p1.getAppointementDate());
     }
-    
+    @Test
+    void testSetters(){
+        p1.setPatientName("Hex");
+        p1.setAge(19);
+        p1.setInsuranceStatus(false);
+        p1.setLevelOfEmergency(5);
+        p1.setspecialistRequired("Dermatologist");
+        p1.setAppointmentDate(LocalDate.of(2024,10,17));
+
+        assertEquals("Hex", p1.getPatientName());
+        assertEquals(19, p1.getAge());
+        assertFalse(p1.getInsuranceStatus());
+        assertEquals(5, p1.getLevelOfEmergency());
+        assertEquals("Dermatologist", p1.getspecialistRequired());
+        assertEquals(LocalDate.of(2024,10,17), p1.getAppointementDate());
+    }
+    @Test
+    void testToCompareSameEmergencySameSpecialist(){
+       assertEquals(0, p1.compareTo(p4)); 
+    }
+
+    @Test
+    void testToCompareHigherEmergencyFirst(){
+       assertTrue(p2.compareTo(p3)<0);
+       assertTrue(p3.compareTo(p2)>0);
+    }
+
+    @Test 
+    void testToCompareForDifferentSpecialist(){
+        assertEquals(0,p1.compareTo(p3));
+    }
+
 
 }
