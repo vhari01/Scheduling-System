99c8969092198b37812d03db27a199d27ec1fbdc
vhari01 <vhari01@student.ubc.ca>
2024-10-10 17:44:15 -0700

    Tested the patient constructor

--- src/test/model/TestPatient.java
+++ src/test/model/TestPatient.java
@@ -17,16 +17,21 @@ public class TestPatient {
 
     @BeforeEach
     void runBefore(){
-        
+        p1 = new Patient("Max",23, true, 3, "Orthopedic", LocalDate.of(2024, 10, 13));
+        p2 = new Patient("Jack",50, false, 5, "Cardiologist", LocalDate.of(2024, 10, 13));
+        p3 = new Patient("Gil",38, true, 1, "Dentist", LocalDate.of(2024, 10, 13));
     }
+
 
     @Test
     void testConstructor() {
-        
-
-
-
-
-    } 
+        assertEquals("Max", p1.getPatientName());
+        assertEquals(23, p1.getAge());
+        assertTrue(p1.getInsuranceStatus());
+        assertEquals("Orthopedic", p1.getspecialistRequired());
+        assertEquals(3, p1.getLevelOfEmergency());
+        assertEquals(LocalDate.of(2024, 10, 13), p1.getAppointementDate());
+    }
+    
 
 }
