f4d93b04996835fdecd7668e80d186ecee35ee41
vhari01 <vhari01@student.ubc.ca>
2024-10-19 14:38:42 -0700

    Created a Specialist class

---
+++ src/main/model/Specialist.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Specialist {
+    
+
+    public Specialist(String specialistName){
+        
+    }
+
+    public void setSpecialist(String specialistName){
+        
+    }
+
+    public String getSpecialist(){
+        return null;
+    }
+
+}
