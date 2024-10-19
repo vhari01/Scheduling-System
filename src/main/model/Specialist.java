ba20241c27bbd93f5d59bd2094d825a86a4f5511
vhari01 <vhari01@student.ubc.ca>
2024-10-19 14:46:36 -0700

    Implemented the Specialist class

--- src/main/model/Specialist.java
+++ src/main/model/Specialist.java
@@ -1,18 +1,21 @@
 package model;
 
 public class Specialist {
-    
+    private String specialistName;
 
+    //REQUIRES: specialistName to not be null
+    //EFFECTS: Creates a specialist object with a String name
     public Specialist(String specialistName){
+        this.specialistName = specialistName;
         
     }
 
     public void setSpecialist(String specialistName){
-        
+        this.specialistName = specialistName;
     }
 
     public String getSpecialist(){
-        return null;
+        return  this.specialistName;
     }
 
 }
