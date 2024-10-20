1cb2b29dc109767c3bb2499c3f434c850a2a718d
vhari01 <vhari01@student.ubc.ca>
2024-10-20 01:12:05 -0700

    Changed the date for the patient objects

--- src/test/model/TestClass.java
+++ src/test/model/TestClass.java
@@ -16,12 +16,12 @@ public class TestClass{
 
     @BeforeEach
     void runBefore(){
-        p1 = new Patient("Max",23, true, 3, Orthopedic, LocalDate.of(2024, 10, 13));
-        p2 = new Patient("Jack",50, false, 5, Cardiologist, LocalDate.of(2024, 10, 13));
-        p3 = new Patient("Gil",38, true, 3, Cardiologist, LocalDate.of(2024, 10, 13));
-        p4 = new Patient("cacy",32, true, 3, Orthopedic, LocalDate.of(2024, 10, 13));
-        p5 = new Patient("Alex", 26, true, 4, Orthopedic, LocalDate.of(2024, 10, 13));
-        p6 = new Patient("Kale", 37, false, 5, Orthopedic, LocalDate.of(2024, 10, 13));
+        p1 = new Patient("Max",23, true, 3, Orthopedic, LocalDate.of(2024, 12, 13));
+        p2 = new Patient("Jack",50, false, 5, Cardiologist, LocalDate.of(2024, 12, 13));
+        p3 = new Patient("Gil",38, true, 3, Cardiologist, LocalDate.of(2024, 12, 13));
+        p4 = new Patient("cacy",32, true, 3, Orthopedic, LocalDate.of(2024, 12, 13));
+        p5 = new Patient("Alex", 26, true, 4, Orthopedic, LocalDate.of(2024, 12, 13));
+        p6 = new Patient("Kale", 37, false, 5, Orthopedic, LocalDate.of(2024, 12, 13));
     }
 
 } 
