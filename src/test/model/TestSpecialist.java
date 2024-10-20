e8823be9d531e527f4dd4d0e1732f8c0c35ece18
vhari01 <vhari01@student.ubc.ca>
2024-10-20 00:22:35 -0700

    created a test class for specialist and implemented it

---
+++ src/test/model/TestSpecialist.java
@@ -0,0 +1,32 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestSpecialist {
+     private Specialist specialist;
+
+    @BeforeEach
+    void setUp() {
+        specialist = new Specialist("Cardiologist");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Cardiologist", specialist.getSpecialistName(), "Specialist name should be set correctly.");
+    }
+
+    @Test
+    public void testGetSpecialistName() {
+        assertEquals("Cardiologist", specialist.getSpecialistName(), "Get specialist name should return the correct name.");
+    }
+
+    @Test
+    public void testSetSpecialistName() {
+        specialist.setSpecialistName("Neurologist");
+        assertEquals("Neurologist", specialist.getSpecialistName(), "Set specialist name should change the name correctly.");
+    }
+    
+}
