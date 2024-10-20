5f6e8a300c9d7e8458bdb7d68d28e877aacd33a5
vhari01 <vhari01@student.ubc.ca>
2024-10-19 20:54:30 -0700

    Fixed the test patient class implemented the specialist object

--- src/test/model/TestPatient.java
+++ src/test/model/TestPatient.java
@@ -16,25 +16,26 @@ public class TestPatient extends TestClass {
         assertEquals("Max", p1.getPatientName());
         assertEquals(23, p1.getAge());
         assertTrue(p1.getInsuranceStatus());
-        assertEquals("Orthopedic", p1.getspecialistRequired());
+        assertEquals(Orthopedic, p1.getspecialistRequired());
         assertEquals(3, p1.getLevelOfEmergency());
         assertEquals(LocalDate.of(2024, 10, 13), p1.getAppointementDate());
     }
 
     @Test
     void testSetters() {
+        Specialist newSpecialist = new Specialist("Dermatologist");
         p1.setPatientName("Hex");
         p1.setAge(19);
         p1.setInsuranceStatus(false);
         p1.setLevelOfEmergency(5);
-        p1.setspecialistRequired("Dermatologist");
+        p1.setspecialistRequired(newSpecialist);
         p1.setAppointmentDate(LocalDate.of(2024, 10, 17));
 
         assertEquals("Hex", p1.getPatientName());
         assertEquals(19, p1.getAge());
         assertFalse(p1.getInsuranceStatus());
         assertEquals(5, p1.getLevelOfEmergency());
-        assertEquals("Dermatologist", p1.getspecialistRequired());
+        assertEquals(newSpecialist, p1.getspecialistRequired());
         assertEquals(LocalDate.of(2024, 10, 17), p1.getAppointementDate());
     }
 
