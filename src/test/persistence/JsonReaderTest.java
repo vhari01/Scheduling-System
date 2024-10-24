3f5c40ec5b92a8556ff91fe11ebd25f4cb78984d
vhari01 <vhari01@student.ubc.ca>
2024-10-24 14:37:02 -0700

    Specified test Methods for Jason Reader test class

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,49 @@
+package persistence;
+
+import model.Patient;
+import model.Scheduler;
+import model.Specialist;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonReaderTest {
+
+
+    @BeforeEach
+    void setup(){
+
+    }
+
+    @Test
+    void testReadFile() {
+
+    }
+
+    @Test
+    void testParseScheduler() {
+
+    }
+
+    @Test
+    void testAddPatients() {
+
+    }
+
+    @Test
+    void testAddPatient() {
+
+    }
+
+    @Test
+    void testReadScheduler() {
+
+    }
+}
