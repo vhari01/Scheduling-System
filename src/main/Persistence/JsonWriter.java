3389d7bcbda605584b88b5f3d270067f9cd2f9f0
vhari01 <vhari01@student.ubc.ca>
2024-10-24 16:36:48 -0700

    Specified few more methods in jsonwriter

--- src/main/Persistence/JsonWriter.java
+++ src/main/Persistence/JsonWriter.java
@@ -11,10 +11,27 @@ public class JsonWriter {
     // EFFECTS: constructs writer to write to the destination file
     public JsonWriter(String destination) {
     }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() {
+    }
+
     // REQUIRES: scheduler is not null
     // MODIFIES: this
     // EFFECTS: writes JSON representation of scheduler to file
     public void write(Scheduler scheduler)  {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
     }
 
     // EFFECTS: converts scheduler to JSON object
@@ -25,7 +42,7 @@ public class JsonWriter {
     private JSONArray patientsToJson(Scheduler scheduler) {
     }
     // REQUIRES: patient is not null
-    // EFFECTS: converts a patient to JSON object
+    // EFFECTS: converts a patient object to JSON object
     private JSONObject patientToJson(Patient patient) {
     }
 }
