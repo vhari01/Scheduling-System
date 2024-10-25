35f4785c829bd5149f58346a5a0f9316108bc783
vhari01 <vhari01@student.ubc.ca>
2024-10-24 17:01:12 -0700

    Specified methods in the jsonwriter test class

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,5 +1,21 @@
 package Persistence;
 
+import org.junit.jupiter.api.Test;
+
 public class JsonWriterTest {
+    
+    @Test
+    void testWriterInvalidFile() {
+    }
+
+    @Test
+    void testWriterEmptyScheduler() {
+    }
+
+    @Test
+    void testWriterGeneralScheduler() {
+    }
+    
+
     
 }
